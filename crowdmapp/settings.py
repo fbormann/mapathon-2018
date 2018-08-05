abddc31d77529f4072078d40eecd2d6016185edb
fbormann <felipe.bormann@gmail.com>
2018-08-05 15:35:11 -0300

    update to debug

--- crowdmapp/settings.py
+++ crowdmapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z9tf8r*fe%yg^=^23!+1ktk(rm1os=2jf(7tw!j#ujj+fe7cta'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
